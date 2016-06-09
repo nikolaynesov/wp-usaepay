3459e2593fc6389e395144965802d0fbf62f3162
= <nikolaynesov@gmail.com>
2016-06-09 18:56:21 +0300

    Update initialization

--- usaepay.php
+++ usaepay.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: USAePay Service Provider
 Plugin URI: 
-Description: USAePay Service Provider.
+Description: Merchants Source key must be generated within the console Service Provider.
 Version: 1.0
 Author: Nikolay Nesov
 Author URI: 
@@ -11,8 +11,8 @@ Author URI:
 $usaepay_base   = plugin_dir_path(__FILE__);
 $usaepay_config = require_once $usaepay_base . 'config/config.php';
 
-require_once $usaepay_base . 'usaepay/Registry.php';
-require_once $usaepay_base . 'usaepay/Init.php';
 require_once $usaepay_base . 'includes/functions.php';
+require_once $usaepay_base . 'usaepay/Admin.php';
+require_once $usaepay_base . 'vendor/autoload.php';
 
-$usaepay_init = new usaepay\Init($usaepay_config);
+$usaepay_init = new usaepay\Admin($usaepay_config);
