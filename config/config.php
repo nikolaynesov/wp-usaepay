d6709ab291ee0e6f3c298b5a9bf174cbdad52f2b
= <nikolaynesov@gmail.com>
2016-06-09 18:55:13 +0300

    Clear

--- config/config.php
+++ config/config.php
@@ -1,23 +1,5 @@
 <?php
 
 	return [
-
-
-		/*
-		 *
-		 * Merchants Source key must be generated within the console
-		 *
-		 */
-
-		'key' => '',
-
-		/*
-		 *
-		 * Send request to sandbox server not production.  Make sure to comment or remove this line before
-		 * putting your code into production
-		 *
-		 */
-
-		'usesandbox' => true
 		
 	];
