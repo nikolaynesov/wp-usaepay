4fa38010c7a335fe4621eceaf487e826e3e0f192
= <nikolaynesov@gmail.com>
2016-06-09 17:58:22 +0300

    Update config

--- config/config.php
+++ config/config.php
@@ -1,6 +1,23 @@
 <?php
 
 	return [
-		
+
+
+		/*
+		 *
+		 * Merchants Source key must be generated within the console
+		 *
+		 */
+
+		'key' => '',
+
+		/*
+		 *
+		 * Send request to sandbox server not production.  Make sure to comment or remove this line before
+		 * putting your code into production
+		 *
+		 */
+
+		'usesandbox' => true
 		
 	];
